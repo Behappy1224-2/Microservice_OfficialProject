85b80dbd2dcb64c8da80ff5c57f9938d8f626559
Jeff <yu410261218@gmail.com>
2024-09-17 06:22:28 +0800

    Add Zero shot classification

--- im_microservice/handlers/command_processor.py
+++ im_microservice/handlers/command_processor.py
@@ -1,9 +1,17 @@
+from transformers import pipeline
+
+# Load the zero-shot classification model
+classifier = pipeline('zero-shot-classification', model='facebook/bart-large-mnli')
+
 def classify_command(message_text):
-    """Classify the command based on the input text."""
-    message_text = message_text.lower()
-    if "turn on" in message_text and "light" in message_text:
-        return 'turn on the light'
-    elif "turn off" in message_text and "light" in message_text:
-        return 'turn off the light'
-    else:
-        return 'other'
+    """Classify the command using zero-shot classification."""
+    # Define potential command labels
+    labels = ['turn on the light', 'turn off the light', 'other']
+
+    # Use the zero-shot classifier to match the command
+    result = classifier(message_text, candidate_labels=labels)
+
+    # Get the highest scored label
+    predicted_label = result['labels'][0]
+    return predicted_label
+
