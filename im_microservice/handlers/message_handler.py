22c93c0a58c18404f2fc980efc4da4ef7f8f7d84
Jeff <yu410261218@gmail.com>
2024-09-25 04:07:46 +0800

    Test

--- im_microservice/handlers/message_handler.py
+++ im_microservice/handlers/message_handler.py
@@ -9,10 +9,10 @@ def handle_text_message(event):
     # Use command_processor to classify the command
     task = classify_command(message_text)
     if task == 'turn on the light':
-        reply_text = "Turning on the light!!"
+        reply_text = "Turning on the light!!!"
     elif task == 'turn off the light':
         reply_text = "Turning off the light~~"
     else:
-        reply_text = "Sry! I didn't understand that command  :("
+        reply_text = "Sry! I can't understand that command :("
 
     send_reply_message(event['replyToken'], reply_text)
