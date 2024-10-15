173e450a21ec6fae0267fcf08f32f3b07d05ad56
Jeff <yu410261218@gmail.com>
2024-10-15 21:36:35 +0800

    IoT control

--- im_microservice/handlers/message_handler.py
+++ im_microservice/handlers/message_handler.py
@@ -1,19 +1,22 @@
+import requests
+import os
 from .api_client import send_reply_message
 from .command_processor import classify_command
-#from iot_test.iot import turn_on_light, turn_off_light
+
+pi_url = os.getenv('pi_url', 'default_token_if_not_set')
 
 def handle_text_message(event):
     user_id = event['source']['userId']
     message_text = event['message']['text']
     print(f"Received text message from {user_id}: {message_text}")
-
+    control_url = f"{pi_url}/control_light"
     # Use command_processor to classify the command
     task = classify_command(message_text)
     if task == 'turn on the light':
-        #turn_on_light()
+        requests.post(control_url, json={'action': 'turn on the light'})
         reply_text = "Turning on the light!!!"
     elif task == 'turn off the light':
-        #turn_off_light()
+        requests.post(control_url, json={'action': 'turn off the light'})
         reply_text = "Turning off the light~~"
     else:
         reply_text = "Sry! I can't understand this command!!! :("
--- iot_test/iot.py
+++ iot_test/iot.py
@@ -1,23 +1,33 @@
+from flask import Flask, request
 import RPi.GPIO as GPIO
-import time
 
-# GPIO setup
-RELAY_PIN = 17  # Use GPIO17 (pin 11 on the Pi)
-GPIO.setmode(GPIO.BCM)  # Set GPIO pin numbering
-GPIO.setup(RELAY_PIN, GPIO.OUT)  # Set the relay pin as an output
+app = Flask(__name__)
 
-def turn_on_light():
-    GPIO.output(RELAY_PIN, GPIO.LOW)  # Relay ON (LOW closes the switch)
-    print("Light is ON")
+# Setup the GPIO pin for controlling the light
+RELAY_PIN_1 = 17  # GPIO pin for IN1
+RELAY_PIN_2 = 27  # GPIO pin for IN2
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(RELAY_PIN_1, GPIO.OUT)
+GPIO.setup(RELAY_PIN_2, GPIO.OUT)
 
-def turn_off_light():
-    GPIO.output(RELAY_PIN, GPIO.HIGH)  # Relay OFF (HIGH opens the switch)
-    print("Light is OFF")
+@app.route('/control_light', methods=['POST'])
+def control_light():
+    data = request.get_json()
+    action = data.get('action')
+    if action == 'turn on the light':
+        GPIO.output(RELAY_PIN_1, GPIO.HIGH)  # Turn on relay channel 1
+        return "Light turned on", 200
+    elif action == 'turn off the light':
+        GPIO.output(RELAY_PIN_1, GPIO.LOW)  # Turn off relay channel 1
+        return "Light turned off", 200
+    elif action == 'turn on another device':
+        GPIO.output(RELAY_PIN_2, GPIO.HIGH)  # Turn on relay channel 2
+        return "Another device turned on", 200
+    elif action == 'turn off another device':
+        GPIO.output(RELAY_PIN_2, GPIO.LOW)  # Turn off relay channel 2
+        return "Another device turned off", 200
+    else:
+        return "Invalid action", 400
 
-try:
-    # Test turning the light on and off
-    turn_on_light()
-    time.sleep(2)  # Keep the light on for 2 seconds
-    turn_off_light()
-finally:
-    GPIO.cleanup()  # Clean up the GPIO pins
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
